2933a81341d4a570cec51adc4c2c3facbd4b6ac6
Piotr Klupa <piiter.klupcia@gmail.com>
2023-04-26 19:11:01 +0200

    Support normalizing relationships

--- index.js
+++ index.js
@@ -25,6 +25,60 @@ function normalize(response) {
   return normalizeSingleObject(response.data);
 };
 
+function buildSingleRelationship(resourceKey, data, included) {
+  var includedData = included.find(function(included) {
+    return included.id === data.id && included.type === data.type;
+  });
+
+  var result = normalizeSingleObject(includedData);
+
+  return {
+    key: resourceKey,
+    data: result,
+  }
+}
+
+function buildRelationships(relationships, included) {
+  var flattenedRelationships = Object.keys(relationships).map(function(resourceKey) {
+    var data = relationships[resourceKey].data;
+
+    if (Array.isArray(data)) {
+      return data.map(function(dataItem) {
+        return buildSingleRelationship(resourceKey, dataItem, included);
+      });
+    }
+
+    return buildSingleRelationship(resourceKey, data, included);
+  });
+
+  return flattenedRelationships.reduce(function(acc, obj) {
+    if (Array.isArray(obj)) {
+      acc[obj[0].key] = obj.map(function(objItem) {
+        return objItem.data;
+      });
+    } else {
+      acc[obj.key] = obj.data;
+    }
+
+    return acc;
+  }, {});
+}
+
+function normalizeRelationships(id, type, response) {
+  if (!id || !type || !response.data) {
+    return {};
+  }
+
+  const data = response.data.find(function(dataItem) {
+    return dataItem.id === id && dataItem.type === type;
+  });
+
+  return buildRelationships(data.relationships, response.included);
+}
+
+
+
 module.exports = {
-  normalize: normalize
+  normalize: normalize,
+  normalizeRelationships: normalizeRelationships,
 };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
 var normalize = require('./index.js').normalize;
+var normalizeRelationships = require('./index.js').normalizeRelationships;
 
 describe('normalize', function() {
   it('should normalize single resource object', function() {
@@ -65,5 +66,78 @@ describe('normalize', function() {
     };
 
     expect(normalize(response)).toEqual({});
+  });
+});
+
+describe('normalizeRelationships', function() {
+  it('should normalize relationships', function() {
+    var response = {
+      data: [{
+        id: '1',
+        type: 'payment_cards',
+        attributes: {
+          last4: '1234',
+        },
+        relationships: {
+          user: {
+            data: {
+              id: '9',
+              type: 'users',
+            },
+          },
+          comments: {
+            data: [
+              { type: 'comments', id: '5' },
+              { type: 'comments', id: '12' },
+            ]
+          },
+        },
+      }],
+      included: [
+        {
+          id: '9',
+          type: 'users',
+          attributes: {
+            first_name: 'Piotr',
+            last_name: 'Klupa',
+          },
+        },
+        {
+          id: '5',
+          type: 'comments',
+          attributes: {
+            body: 'First!',
+          },
+        },
+        {
+          id: '12',
+          type: 'comments',
+          attributes: {
+            body: 'Second!',
+          },
+        }
+      ]
+    };
+
+    expect(normalizeRelationships('1', 'payment_cards', response)).toEqual({
+      user: {
+        id: '9',
+        type: 'users',
+        first_name: 'Piotr',
+        last_name: 'Klupa',   
+      },
+      comments: [
+        {
+          id: '5',
+          type: 'comments',
+          body: 'First!',
+        },
+        {
+          id: '12',
+          type: 'comments',
+          body: 'Second!',
+        },
+      ]
+    });
   });
 });
